7a84b05814837b53d9b9938967d5da6647939e96
Kunal493 <kunalgelot555@gmail.com>
2024-08-11 18:35:21 +0530

    create add_book function

--- library.py
+++ library.py
@@ -7,3 +7,16 @@ class Library:
         Initialize a new library instance with an empty collection of books.
         """
         self.books = {}
+
+    def add_book(self, book: Book):
+        """
+        Add a book to the library collection.
+        :param book: An instance of the Book class.
+        """
+        if not isinstance(book, Book):
+            raise TypeError("Invalid book type.")
+        
+        if book.isbn in self.books:
+            self.books[book.isbn].total_book_copies += 1
+        else:
+            self.books[book.isbn] = book
